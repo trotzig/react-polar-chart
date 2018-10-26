6d817c9983982c50b8f22d8261eca52bfd0f25ed
Henric Trotzig <henric.trotzig@gmail.com>
2018-10-26 23:29:29 +0200

    Fix rendering in IE11
    An svg needs a height in order to render nicely in IE. We can use the
    fact that the component is always 1/1 and apply padding plus absolute
    positioning.
    
    As a nice side-effect, other browsers suddenly rendered the inner circle
    at a better position (it was slightly off before).

--- src/PolarChart.js
+++ src/PolarChart.js
@@ -68,108 +68,116 @@ export default function PolarChart({ maxScore, score, slices, activeSlice, onSli
   const gap = CENTER / (NUMBER_OF_INDICATING_CIRCLES + 1);
   const degPerSlice = 360 / slices.length;
   return (
-    <div
-      className="PolarChart"
-      style={{
-        position: 'relative',
-      }}
-    >
-      <style>{`
-        .PolarChart path {
-          cursor: pointer;
-        }
-        .PolarChart path:hover {
-          opacity: 1 !important;
-        }
-        .PolarChart-innerCircle {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          border-radius: 50%;
-          background-color: #fff;
-          height: 20%;
-          width: 20%;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          justify-content: center;
-          cursor: pointer;
-          font-size: ${SIZE / 4}px;
-          font-weight: 500;
-          line-height: 1;
-          text-align: center;
-        }
-        .PolarChart-innerCircle span {
-          font-size: ${SIZE / 9}px;
-          opacity: 0.5;
-          display: block;
-          line-height: 1;
-        }
-        .PolarChart-label {
-          height: 50%;
-          position: absolute;
-          left: 50%;
-          top: 0;
-          transform-origin: bottom center;
-          font-weight: bold;
-          width: 0;
-        }
-      `}</style>
-      {slices.map((slice, i) => {
-        const { label, color } = slice;
-        const deg = (360 * (i / slices.length)) - (degPerSlice / 2);
-        let opacity = 1;
-        if (typeof activeSlice !== 'undefined' && i !== activeSlice) {
-          opacity = 0.5;
-        }
-        return (
-          <div
-            className="PolarChart-label"
-            style={{
-              color,
-              transform: `rotate(${deg}deg)`,
-            }}
-          >
+    <div className="PolarChart">
+      <div className="PolarChart-inner">
+        <style>{`
+          .PolarChart {
+            position: relative;
+            padding-top: 100%;
+          }
+          .PolarChart-inner {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+          }
+          .PolarChart path {
+            cursor: pointer;
+          }
+          .PolarChart path:hover {
+            opacity: 1 !important;
+          }
+          .PolarChart-innerCircle {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border-radius: 50%;
+            background-color: #fff;
+            height: 20%;
+            width: 20%;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            font-size: ${SIZE / 4}px;
+            font-weight: 500;
+            line-height: 1;
+            text-align: center;
+          }
+          .PolarChart-innerCircle span {
+            font-size: ${SIZE / 9}px;
+            opacity: 0.5;
+            display: block;
+            line-height: 1;
+          }
+          .PolarChart-label {
+            height: 50%;
+            position: absolute;
+            left: 50%;
+            top: 0;
+            transform-origin: bottom center;
+            font-weight: bold;
+            width: 0;
+          }
+        `}</style>
+        {slices.map((slice, i) => {
+          const { label, color } = slice;
+          const deg = (360 * (i / slices.length)) - (degPerSlice / 2);
+          let opacity = 1;
+          if (typeof activeSlice !== 'undefined' && i !== activeSlice) {
+            opacity = 0.5;
+          }
+          return (
             <div
+              className="PolarChart-label"
               style={{
-                transform: `translateX(-50%) rotate(${360 - deg}deg)`,
-                cursor: 'pointer',
-                opacity,
+                color,
+                transform: `rotate(${deg}deg)`,
               }}
-              onClick={() => onSliceSelected(slice, i)}
             >
-              {label}
+              <div
+                style={{
+                  transform: `translateX(-50%) rotate(${360 - deg}deg)`,
+                  cursor: 'pointer',
+                  opacity,
+                }}
+                onClick={() => onSliceSelected(slice, i)}
+              >
+                {label}
+              </div>
             </div>
+          );
+        })}
+        <svg viewBox={`0 0 ${SIZE} ${SIZE}`}>
+          <g transform={`rotate(-90 ${CENTER} ${CENTER})`}>
+            {renderPaths({ slices, onSliceSelected, activeSlice })}
+          </g>
+          {[...Array(NUMBER_OF_INDICATING_CIRCLES)].map((_, index) => (
+            <circle
+              key={index}
+              cx={CENTER}
+              cy={CENTER}
+              r={(gap / 2) + (gap * (index + 1))}
+              stroke="#fff"
+              strokeWidth="0.8"
+              fill="none"
+              style={{ pointerEvents: 'none' }}
+            />
+          ))}
+        </svg>
+        <div
+          className="PolarChart-innerCircle"
+          onClick={() => onSliceSelected(undefined, undefined)}
+        >
+          <div>
+            {score}
+            <span>
+              /{maxScore}
+            </span>
           </div>
-        );
-      })}
-      <svg viewBox={`0 0 ${SIZE} ${SIZE}`}>
-        <g transform={`rotate(-90 ${CENTER} ${CENTER})`}>
-          {renderPaths({ slices, onSliceSelected, activeSlice })}
-        </g>
-        {[...Array(NUMBER_OF_INDICATING_CIRCLES)].map((_, index) => (
-          <circle
-            key={index}
-            cx={CENTER}
-            cy={CENTER}
-            r={(gap / 2) + (gap * (index + 1))}
-            stroke="#fff"
-            strokeWidth="0.8"
-            fill="none"
-            style={{ pointerEvents: 'none' }}
-          />
-        ))}
-      </svg>
-      <div
-        className="PolarChart-innerCircle"
-        onClick={() => onSliceSelected(undefined, undefined)}
-      >
-        <div>
-          {score}
-          <span>
-            /{maxScore}
-          </span>
         </div>
       </div>
     </div>
